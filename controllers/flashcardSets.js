56f2c2b9360d58d7d3e0f36289b736c66c24c75c
Marwinez <gietus579@gmail.com>
2026-05-31 12:29:24 +0200

    First commit

---
+++ controllers/flashcardSets.js
@@ -0,0 +1,64 @@
+const flashcardSetsRouter = require('express').Router()
+const FlashcardSet = require('../models/flashcardSet')
+
+flashcardSetsRouter.get('/', (req, res) => {
+    FlashcardSet.find({}).then(flashcardSets => {
+        res.json(flashcardSets)
+    })
+})
+
+flashcardSetsRouter.get('/:id', (req, res) => {
+    FlashcardSet.findById(req.params.id)
+        .then(flashcardSet => {
+            if (flashcardSet) {
+                res.json(flashcardSet)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+flashcardSetsRouter.post('/', (req, res) => {
+    const body = req.body
+
+    const flashcardSet = new FlashcardSet({
+        title: body.title,
+        cards: body.cards,
+    })
+
+    flashcardSet.save()
+        .then(savedFlashcardSet => {
+            res.json(savedFlashcardSet)
+        })
+        .catch(error => next(error))
+})
+
+flashcardSetsRouter.delete('/:id', (req, res) => {
+    FlashcardSet.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+flashcardSetsRouter.delete('/:id', (req, res) => {
+    const { title, cards } = req.body
+
+    FlashcardSet.findById(req.params.id)
+        .then(flashcardSet => {
+            if(!flashcardSet) {
+                return res.status(404).end()
+            }
+
+            flashcardSet.title = title
+            flashcardSet.cards = cards
+
+            return flashcardSet.save().then(updatedFlashcardSet => {
+                res.json(updatedFlashcardSet)
+            })
+        })
+        .catch(error => next(error))
+})
+
+module.exports = flashcardSetsRouter
---
+++ models/flashcardSet.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const flashcardSetSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    cards: {
+        type: Array,
+        required: true,
+    }
+})
+
+flashcardSetSchema.set('toJSON', {
+    transform: (doc, ret) => {
+        ret.id = ret._id.toString()
+        delete ret._id
+        return ret.__v
+    }
+})
+
+module.exports = mongoose.model('FlashcardSet', flashcardSetSchema)
